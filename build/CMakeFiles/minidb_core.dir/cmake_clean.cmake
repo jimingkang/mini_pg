3aa8e7e2cd5d7b98740aecb568a4855d2ddd4409
jimingkang <jimingkang@gmail.com>
2025-06-22 06:30:56 +0800

    transation is only for single process ,nof for tcp client

---
+++ build/CMakeFiles/minidb_core.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minidb_core.dir/src/catalog.c.o"
+  "CMakeFiles/minidb_core.dir/src/minidb.c.o"
+  "CMakeFiles/minidb_core.dir/src/page.c.o"
+  "CMakeFiles/minidb_core.dir/src/server/executor.c.o"
+  "CMakeFiles/minidb_core.dir/src/server/parser.c.o"
+  "CMakeFiles/minidb_core.dir/src/server/server.c.o"
+  "CMakeFiles/minidb_core.dir/src/server/sql_exec.c.o"
+  "CMakeFiles/minidb_core.dir/src/tuple.c.o"
+  "CMakeFiles/minidb_core.dir/src/txmgr.c.o"
+  "CMakeFiles/minidb_core.dir/src/wal.c.o"
+  "lib/libminidb_core.a"
+  "lib/libminidb_core.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/minidb_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/minidb_core.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "lib/libminidb_core.a"
+)
